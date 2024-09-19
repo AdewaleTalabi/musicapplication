d4ef02d586b3e37316554f7483a724f4cf93c145
AdewaleTalabi <80199617+AdewaleTalabi@users.noreply.github.com>
2024-09-19 12:52:15 +0100

    Add files via upload

---
+++ scripts/runapp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+echo 'The following "npm" command runs Node.js application'
+
+set -x
+npm start &
+sleep 1
+echo $! > .pidfile
+set +x
+
+echo 'Now you can'
+echo 'Visit http://ServerIp:9981 to see your Node.js application'
